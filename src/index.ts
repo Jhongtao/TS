ada42e8e6f41d00efba81172b52f51361bf3727c
Jhongtao <466795794@qq.com>
2021-02-16 21:39:38 +0800

    enum

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,10 @@
-type g = 'girl'|'boy'
-type U = {
-    name:string
-    age:number
-    gender:g
+enum Leve{
+    leve1=1,
+    leve2,
+    leve3
 }
 
-function getName(name:U):U{
-    return name
+function getLeve(l:Leve):Leve{
+    return l
 }
-console.log(getName({
-    name:'faf',
-    age:12,
-    gender:"boy"
-}))
-type arg = hs[]
-type hs = {
-    name:string
-    age:number
-}
-function createArg(arg):arg
-/** 
-
-@param arg
- arg类型name和age
-  **/
-
-
-
-
-function createArg(arg:arg){
-    // for(let i=0;i<10;i++){
-    //    arg.push({
-    //         name:'XXX',
-    //         age:Math.floor(Math.random()*30)
-    //    })
-    // }
-   return arg
-}
-console.log(createArg([]))
+console.log(getLeve(Leve.leve3))
